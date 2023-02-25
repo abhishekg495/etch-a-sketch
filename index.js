66f63d512859a11511deebd1e3329807731e317b
abhishekg495 <abhishekgupta23810306@gmail.com>
2023-02-26 00:13:44 +0530

    fix grid lines bug

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const canvas = document.querySelector(".canvas");
 const gridSizeSlider = document.querySelector("#grid-size");
 const gridSizeLabel = document.querySelector("label[for='grid-size']");
 const toggleGridLines = document.querySelector("#grid-lines");
+let gridLines = false;
+const clearBtn = document.querySelector("#clear-canvas");
 let pixels = document.querySelectorAll(".pixel");
 let gridSize = gridSizeSlider.value;
 gridSizeLabel.textContent = `Grid Size: ${gridSize}`;
@@ -13,37 +15,52 @@ function createPixel() {
 		if (e.buttons == 1 || e.buttons == 3)
 			e.target.style.backgroundColor = "black";
 	});
+	pixel.addEventListener("click", (e) => {
+		e.target.style.backgroundColor = "black";
+	});
 	return pixel;
 }
-
+function updateGridLines() {
+	if (gridLines) {
+		canvas.style.borderWidth = "0 0 1px 1px";
+		pixels.forEach((pixel) => (pixel.style.borderWidth = "1px 1px 0px 0px"));
+	} else {
+		canvas.style.borderWidth = "1px";
+		pixels.forEach((pixel) => (pixel.style.borderWidth = "0"));
+	}
+}
 function clearCanvas() {
+	pixels.forEach((pixel) => (pixel.style.backgroundColor = "white"));
+}
+function shrinkCanvas() {
 	canvas.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
 	while (canvas.childElementCount > gridSize * gridSize) {
 		canvas.removeChild(canvas.firstChild);
 	}
-	pixels = document.querySelectorAll(".pixel");
-	pixels.forEach((pixel) => (pixel.style.backgroundColor = "white"));
 }
-function fillCanvas() {
+function expandCanvas() {
 	canvas.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
 	while (canvas.childElementCount < gridSize * gridSize) {
 		canvas.appendChild(createPixel());
 	}
-	pixels = document.querySelectorAll(".pixel");
 }
-function createCanvas() {
+function updateCanvas() {
+	shrinkCanvas();
+	expandCanvas();
 	clearCanvas();
-	fillCanvas();
+	pixels = document.querySelectorAll(".pixel");
+	updateGridLines();
 }
 
 gridSizeSlider.addEventListener("input", () => {
 	gridSize = gridSizeSlider.value;
 	gridSizeLabel.textContent = `Grid Size: ${gridSize}`;
-	createCanvas();
+	updateCanvas();
 });
 
 toggleGridLines.addEventListener("click", () => {
-	pixels.forEach((pixel) => pixel.classList.toggle("no-border"));
+	gridLines = !gridLines;
+	updateGridLines();
 });
 
-createCanvas();
+updateCanvas();
