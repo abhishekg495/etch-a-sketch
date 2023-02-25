fbada658e40c86fee0e11dbe16d164d21060ebd4
abhishekg495 <abhishekgupta23810306@gmail.com>
2023-02-25 23:38:36 +0530

    add black paint feature

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const canvas = document.querySelector(".canvas");
-const menu = document.querySelector(".menu");
-const gameContainer = document.querySelector(".game-container");
 const gridSizeSlider = document.querySelector("#grid-size");
 const gridSizeLabel = document.querySelector("label[for='grid-size']");
 const toggleGridLines = document.querySelector("#grid-lines");
@@ -8,21 +6,28 @@ let pixels = document.querySelectorAll(".pixel");
 let gridSize = gridSizeSlider.value;
 gridSizeLabel.textContent = `Grid Size: ${gridSize}`;
 
+function createPixel() {
+	let pixel = document.createElement("div");
+	pixel.classList.add("pixel");
+	pixel.addEventListener("mouseover", (e) => {
+		if (e.buttons == 1 || e.buttons == 3)
+			e.target.style.backgroundColor = "black";
+	});
+	return pixel;
+}
+
 function clearCanvas() {
-	// console.log(canvas.childElementCount);
+	canvas.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
 	while (canvas.childElementCount > gridSize * gridSize) {
 		canvas.removeChild(canvas.firstChild);
 	}
+	pixels = document.querySelectorAll(".pixel");
+	pixels.forEach((pixel) => (pixel.style.backgroundColor = "white"));
 }
 function fillCanvas() {
 	canvas.style.gridTemplateColumns = `repeat(${gridSize},1fr)`;
-	//canvas.style.gridTemplateRows = `repeat(${gridSize},1fr)`;
 	while (canvas.childElementCount < gridSize * gridSize) {
-		let pixel = document.createElement("div");
-		pixel.classList.add("pixel");
-		//pixel.style.width = canvas.style.width / gridSize;
-		//pixel.style.height = canvas.style.height / gridSize;
-		canvas.appendChild(pixel);
+		canvas.appendChild(createPixel());
 	}
 	pixels = document.querySelectorAll(".pixel");
 }
